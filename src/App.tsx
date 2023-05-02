cbb83a7efe3cbe95432d2deebeade92c652e6950
kiryong <116635003+kiryong159@users.noreply.github.com>
2023-05-02 11:10:37 +0900

    8.6

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import styled, { createGlobalStyle } from "styled-components";
 import HelmetComponent from "./helmet";
 import { motion } from "framer-motion";
+import { useRef } from "react";
 
 const GlobalCss = createGlobalStyle`
 body{
@@ -26,25 +27,47 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
+const BigBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: #bdc3c78d;
+  border-radius: 25px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+`;
+
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
-  border-radius: 10px;
+  background-color: #81a8c28d;
+  border-radius: 25px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const boxVars = {
+  hover: { scale: 1.0, rotateZ: 90 },
+  click: { scale: 1.0, borderRadius: "100px" },
+};
+
 function App() {
+  const bigBoxRef = useRef<HTMLDivElement>(null);
   return (
     <>
       <HelmetComponent />
       <GlobalCss />
       <Wrapper>
-        <Box
-          initial={{ scale: 0 }}
-          animate={{ scale: 1, rotateZ: 360 }}
-          transition={{ duration: 0.5, type: "spring", delay: 1 }}
-        />
+        <BigBox ref={bigBoxRef}>
+          <Box
+            drag
+            dragSnapToOrigin
+            dragConstraints={bigBoxRef}
+            variants={boxVars}
+            whileHover="hover"
+            whileTap="click"
+          />
+        </BigBox>
       </Wrapper>
     </>
   );
