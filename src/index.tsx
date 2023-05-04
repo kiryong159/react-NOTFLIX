69179fd9aec373c08b2cc549b62046f8a6686d77
kiryong <116635003+kiryong159@users.noreply.github.com>
2023-05-04 16:56:09 +0900

    9.0
    setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,35 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
 import { darkTheme } from "./theme";
+import { createGlobalStyle } from "styled-components";
+import HelmetComponent from "./helmet";
+
+const GlobalCss = createGlobalStyle`
+body{
+  font-family: "Nunito", sans-serif;
+  line-height: 1.2;
+  color:black
+}
+a {
+  text-decoration-line: none;
+color:inherit;
+}
+* {
+  box-sizing: border-box;
+}
+`;
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <RecoilRoot>
     <ThemeProvider theme={darkTheme}>
+      <HelmetComponent />
+      <GlobalCss />
       <App />
     </ThemeProvider>
   </RecoilRoot>
