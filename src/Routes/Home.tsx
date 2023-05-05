723e7281e524cce5b8af9605f5019edbe40c8363
kiryong <116635003+kiryong159@users.noreply.github.com>
2023-05-05 12:07:57 +0900

    8.2

---
+++ src/Routes/Home.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const A = styled.div`
+  background-color: #ffffff;
+  height: 100vh;
+`;
+
+function Home() {
+  return <A>home</A>;
+}
+
+export default Home;
